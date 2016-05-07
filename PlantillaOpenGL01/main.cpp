92880ef0dcb2430a9c4470c43fd655eaf82eacd7
Edward Fernández <edwardfernandez26@gmail.com>
2016-05-07 01:00:32 -0430

    V-0.0.0

---
+++ PlantillaOpenGL01/main.cpp
@@ -0,0 +1,111 @@
+#include <iostream>
+#include <GL\glew.h>
+#include <GL\freeglut.h>
+
+
+using namespace std;
+
+#define DEF_floorGridScale	1.0f
+#define DEF_floorGridXSteps	10.0f
+#define DEF_floorGridZSteps	10.0f
+
+void ejesCoordenada(float w) {
+	
+	glLineWidth(w);
+	glBegin(GL_LINES);
+		glColor3f(1.0,0.0,0.0);
+		glVertex2f(0,10);
+		glVertex2f(0,-10);
+		glColor3f(0.0,0.0,1.0);
+		glVertex2f(10,0);
+		glVertex2f(-10,0);
+	glEnd();
+
+	glLineWidth(w-1.0);
+	int i;
+	glColor3f(0.0,1.0,0.0);
+	glBegin(GL_LINES);
+		for(i = -10; i <=10; i++){
+			if (i!=0) {		
+				if ((i%2)==0){	
+					glVertex2f(i,0.4);
+					glVertex2f(i,-0.4);
+
+					glVertex2f(0.4,i);
+					glVertex2f(-0.4,i);
+				}else{
+					glVertex2f(i,0.2);
+					glVertex2f(i,-0.2);
+
+					glVertex2f(0.2,i);
+					glVertex2f(-0.2,i);
+
+				}
+			}
+		}
+		
+	glEnd();
+
+	glLineWidth(1.0);
+}
+
+void changeViewport(int w, int h) {
+	glViewport(0,0,w,h);
+}
+
+void render(){
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	GLfloat zExtent, xExtent, xLocal, zLocal;
+    int loopX, loopZ;
+
+	/* Render Grid */
+	glPushMatrix();
+    glColor3f( 0.0f, 0.7f, 0.7f );
+    glBegin( GL_LINES );
+    zExtent = DEF_floorGridScale * DEF_floorGridZSteps;
+    for(loopX = -DEF_floorGridXSteps; loopX <= DEF_floorGridXSteps; loopX++ )
+	{
+	xLocal = DEF_floorGridScale * loopX;
+	glVertex3f( xLocal, -zExtent, 0.0f );
+	glVertex3f( xLocal, zExtent,  0.0f );
+	}
+    xExtent = DEF_floorGridScale * DEF_floorGridXSteps;
+    for(loopZ = -DEF_floorGridZSteps; loopZ <= DEF_floorGridZSteps; loopZ++ )
+	{
+	zLocal = DEF_floorGridScale * loopZ;
+	glVertex3f( -xExtent, zLocal, 0.0f );
+	glVertex3f(  xExtent, zLocal, 0.0f );
+	}
+    glEnd();
+    glPopMatrix();
+
+
+	glutSwapBuffers();
+}
+
+int main (int argc, char** argv) {
+
+	glutInit(&argc, argv);
+
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA | GLUT_DEPTH);
+
+	glutInitWindowSize(800,600);
+
+	glutCreateWindow("Opengl");
+
+	glutReshapeFunc(changeViewport);
+	glutDisplayFunc(render);
+
+	
+	GLenum err = glewInit();
+	if (GLEW_OK != err) {
+		fprintf(stderr, "GLEW error");
+		return 1;
+	}
+	
+
+	glutMainLoop();
+	return 0;
+
+}
